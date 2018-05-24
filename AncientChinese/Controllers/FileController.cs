0fcc489c118c5b9c49820fff06f4c604c6418997
Sean Zheng <Sean_Zheng.NULL@outlook.com>
2018-05-24 21:45:37 +0800

    文件返回接口

--- AncientChinese/Controllers/FileController.cs
+++ AncientChinese/Controllers/FileController.cs
@@ -1,13 +1,39 @@
-﻿using System;
+﻿using AncientChinese.Models.SqlOperater;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace AncientChinese.Controllers
 {
     public class FileController : ApiController
     {
+        [HttpGet]
+        public HttpResponseMessage Image(string id)
+        {
+            FileOperater operater = new FileOperater();
+            string fileName = operater.GetFilePath(id);
+            if (fileName != null)
+            {
+                //string path = System.Web.Hosting.HostingEnvironment.MapPath(fileName);
+                string path = System.Web.Hosting.HostingEnvironment.MapPath($"~/Resources/{fileName}");
+                Stream stream = File.Open(path, FileMode.Open);
+                HttpResponseMessage message = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StreamContent(stream),
+                };
+                message.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(MimeMapping.GetMimeMapping(fileName));
+                return message;
+            }
+            else
+            {
+                HttpResponseMessage message = new HttpResponseMessage(HttpStatusCode.NotFound);
+                return message;
+            }
+        }
     }
 }
---
+++ AncientChinese/Models/SqlOperater/FileOperater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AncientChinese.Models.SqlOperater
+{
+    public class FileOperater:DbOperater
+    {
+        public string GetFilePath(string fileId)
+        {
+            using (SqlCommand command=_connection.CreateCommand())
+            {
+                command.CommandText = "SELECT Tb_File.FileName FROM Tb_File WHERE FileId=@id";
+                command.Parameters.AddWithValue("@id", fileId);
+
+                string path = null;
+                SqlDataReader reader = GetSqlDataReader(command);
+                if (reader.Read())
+                    path = reader.GetString(0);
+                CloseConnection();
+                return path;
+            }
+        }
+    }
+}
